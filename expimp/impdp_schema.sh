064f51db74bee76748c320db40174af14bb045ee
nyasuhiro <runners.high1028@gmail.com>
2019-06-16 00:42:56 +0900

    add impdp script

---
+++ expimp/impdp_schema.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+export ORACLE_SID=nyadb
+export NLS_LANG=American_America.AL32UTF8
+
+ADMINUSER=NYADBA
+ADMINPASS=nyadba
+IMPUSER=SOE
+TNS=nyapdb1
+
+DMP_PATH=/u01/app/oracle/dpdump
+DIROBJ_NAME=IMP_DIR
+
+MODE=ALL
+TS_ACTION=SKIP
+
+TIMESTAMP=`date +"%Y%m%d_%H%M%S"`
+BASE_NAME=${ORACLE_SID}_${IMPUSER}_${MODE}
+DMP_NAME=${BASE_NAME}.dmp
+LOG_NAME=${BASE_NAME}_${TIMESTAMP}.imp.log
+JOB_NAME=JOB_${IMPUSER}_IMPSCHEMA
+
+sqlplus ${ADMINUSER}/${ADMINPASS}@${TNS} << EOF | tee -a ${LOG_NAME}
+set echo on
+set feed off
+drop user ${IMPUSER} cascade;
+EOF
+
+sqlplus -s ${ADMINUSER}/${ADMINPASS}@${TNS} << EOF | tee -a ${LOG_NAME}
+set feed off
+set line 1000
+set pages 1000
+col OWNER for a30
+col DIRECTORY_NAME for a30
+col DIRECTORY_PATH for a100
+create or replace DIRECTORY ${DIROBJ_NAME} AS '${DMP_PATH}';
+select DIRECTORY_NAME,DIRECTORY_PATH from DBA_DIRECTORIES where DIRECTORY_NAME='${DIROBJ_NAME}';
+EOF
+
+impdp ${ADMINUSER}/${ADMINPASS}@${TNS} \
+  SCHEMAS=${IMPUSER} \
+  DIRECTORY=${DIROBJ_NAME} \
+  DUMPFILE=${DMP_NAME} \
+  LOGFILE=${LOG_NAME} \
+  CONTENT=${MODE} \
+  LOGTIME=ALL \
+  TABLE_EXISTS_ACTION=${TS_ACTION} \
+  METRICS=YES  
+
+sqlplus -s ${ADMINUSER}/${ADMINPASS}@${TNS} << EOF
+set feed off
+drop DIRECTORY ${DIROBJ_NAME};
+EOF
+
+ls -l ${DMP_PATH}/${BASE_NAME}*
+cat ${DMP_PATH}/${LOG_NAME} >> ${LOG_NAME}
