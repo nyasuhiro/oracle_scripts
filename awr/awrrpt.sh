4b3f370bb43b3f43e5f90d57807ebd68fca3644c
nyasuhiro <runners.high1028@gmail.com>
2019-06-16 22:04:57 +0900

    変数名をリファクタリング

--- awr/awrrpt.sh
+++ awr/awrrpt.sh
@@ -12,16 +12,16 @@ select MAX(SNAP_ID) from awr_pdb_snapshot;
 exit;
 EOF
 `
-export SNAP_BEFORE=$((${SNAP_TMP}-1))
-export SNAP_AFTER=$((${SNAP_TMP}))
+export BEGIN_SNAP=$((${SNAP_TMP}-1))
+export END_SNAP=$((${SNAP_TMP}))
 
 sqlplus -s ${USER}/${PASS}@${TNS} << EOF
 define report_type=text;
 define awr_location=AWR_PDB;
 define num_days=1;
-define begin_snap=${SNAP_BEFORE};
-define end_snap=${SNAP_AFTER};
-define report_name=awrrpt_${TNS}_${SNAP_BEFORE}_${SNAP_AFTER}.log;
+define begin_snap=${BEGIN_SNAP};
+define end_snap=${END_SNAP};
+define report_name=awrrpt_${TNS}_${BEGIN_SNAP}_${END_SNAP}.log;
 @${ORACLE_HOME}/rdbms/admin/awrrpt.sql;
 EOF
 
