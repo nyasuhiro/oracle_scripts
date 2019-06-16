ed0bb023e567073863ad7dfbd5b7548b8a050b1e
nyasuhiro <runners.high1028@gmail.com>
2019-06-16 22:04:29 +0900

    snapshot取得をシェルで完結するよう修正

--- awr/snapshot.sh
+++ awr/snapshot.sh
@@ -4,6 +4,26 @@ export ORACLE_SID=nyadb
 USER=NYADBA
 PASS=nyadba
 TNS=nyapdb1
-SQLFILE=snapshot.sql
 
-sqlplus ${USER}/${PASS}@${TNS} @${SQLFILE}
+sqlplus -s ${USER}/${PASS}@${TNS} << EOF
+set lines 120
+set pages 100
+set echo on
+set time on
+set timing on
+clear col
+column log_date new_value log_date noprint
+select to_char(sysdate,'yyyymmdd_HH24MISS') log_date from dual;
+spool snapshot_&log_date..log
+select 
+  MAX(SNAP_ID) 
+from 
+  AWR_PDB_SNAPSHOT;
+exec DBMS_WORKLOAD_REPOSITORY.CREATE_SNAPSHOT ();
+select 
+  MAX(SNAP_ID)
+from 
+  AWR_PDB_SNAPSHOT;
+spool off
+exit
+EOF
